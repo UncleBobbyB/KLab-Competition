2c71a532313828bf12643424ac1410562b8c3abd
UncleBobbyB <jyinj@outlook.com>
2019-05-10 19:34:16 +0800

    -

---
+++ funs.py
@@ -0,0 +1,14 @@
+def process_review(raw_review):
+    """Function to convert a raw review to a string of words
+    
+    Args:
+        raw_review: single string
+
+    Returns:
+        single string, which is a processed review
+    """
+    # Convert to lower case
+    review = raw_review.lower()
+
+    return review
+        
